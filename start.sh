17fdafb2165c099af0aefb1b4a56b2af4a0fb1f3
Moon-and-you <2292752593@qq.com>
2021-11-13 00:24:21 +0800

    xxx

--- start.sh
+++ start.sh
@@ -5,3 +5,8 @@ cd ../MiguMusicApi/
 pm2 start --name migu_music npm -- start
 cd ../NeteaseCloudMusicApi/
 pm2 start --name netease_music app.js
+
+kill -9 `ps -ef|grep main|grep -v grep|awk '{print $2}'`
+cd ../goServer/
+go build main.go
+nohup ./main >/dev/null 2>&1 &
