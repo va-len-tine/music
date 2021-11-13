05c5c35d69d8b478face0df9bb63529295c9e80c
liwen <2292752593@qq.com>
2021-11-13 16:59:05 +0800

    add html

---
+++ music/components/jm-search/jm-search.vue
@@ -0,0 +1,168 @@
+<template>
+	<view>
+		<!-- 搜索条 -->
+		<view class="searchTopBox">
+			<view class="searchBoxRadius">
+				<view class="grace-search-icon searchBoxIcon"></view>
+				<input class="searchBoxIpt" type="search" v-model="ipt" @confirm="searchNow($event)" placeholder="歌名/歌手"></input>
+			</view>
+		</view>
+		<!-- 搜索历史 -->
+		<view v-if=false class="searchBotBox">
+			<view class="ov">
+				<view class="fl">搜索历史</view>
+				<view @tap="clearKey" class="fr grace-more-r grace-search-remove">删除</view>
+			</view>
+			<view class="searchHistoryBox">
+				<view class="searchHistoryBoxItem" v-for="(item,index) in searchKey" :key='index'>
+					{{item}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				searchKey: [],
+				ipt: '',
+				searchClose: true,
+			}
+		},
+		onShow() {
+			var vv = uni.getStorageSync('searchLocal');
+			var arr = vv.split("-");
+			this.searchKey = arr;
+		},
+		methods: {
+
+			clearKey: function() {
+				var that = this;
+				uni.showModal({
+					title: '提示',
+					content: '点击确定将删除所有历史信息，确定删除吗？',
+					success: function(res) {
+						if (res.confirm) {
+							that.searchKey = [];
+							uni.setStorage({
+								key: 'searchLocal',
+								data: that.searchKey
+							});
+							
+						} else if (res.cancel) {
+
+						}
+					}
+				});
+
+			},
+			searchNow: function(e) {
+				if (this.ipt == '') {
+					uni.showToast({
+						title: '未输入搜索关键字',
+						icon: 'none',
+						duration: 1000
+					});
+					return false;
+				}
+				var that = this;
+				var newArr = that.searchKey;
+				newArr.push(this.ipt);
+				if (newArr.length == 6){
+					newArr.shift()
+				}
+				this.searchKey = newArr;
+				var newStr = newArr.join('-');
+				uni.setStorage({
+					key: 'searchLocal',
+					data: newStr
+				});
+				this.$emit('flush_data', this.ipt)
+				// uni.request({
+				// 	url:'http://39.101.203.25/' + this.search_type + '/api?keyword=' + this.ipt,
+				// 	method:'GET',
+				// 	data:{},
+				// 	success: (res) => {
+				// 		this.$emit('flush_data', res)
+				// 	}
+				// });
+			}
+		}
+	}
+</script>
+<style>
+	page {
+		background: #FFF;
+	}
+
+	.ov {
+		overflow: hidden;
+	}
+
+	.fl {
+		float: left;
+	}
+
+	.fr {
+		float: right;
+	}
+
+	.searchTopBox {
+		width: 100%;
+		background-color: #877383;
+		height: 100upx;
+		box-sizing: border-box;
+		padding-top: 15upx;
+	}
+
+	.searchBoxRadius {
+		width: 90%;
+		height: 70upx;
+		background-color: #ffffff;
+		margin-left: 5%;
+		overflow: hidden;
+		border-radius: 35upx;
+	}
+
+	.searchBoxIcon {
+		font-size: 40upx;
+		margin-top: 20upx;
+		margin-left: 20upx;
+		float: left;
+	}
+
+	.searchBoxIpt {
+		height: 70upx;
+		line-height: 70upx;
+		margin-left: 20upx;
+		float: left;
+	}
+
+	.searchBotBox {
+		width: 100%;
+		margin-top: 30upx;
+		padding: 15upx 3%;
+		box-sizing: border-box;
+	}
+
+	.searchHistoryBox {
+		width: 100%;
+		box-sizing: border-box;
+		overflow: hidden;
+		margin-top: 40upx;
+	}
+
+	.searchHistoryBoxItem {
+		float: left;
+		font-size: 26upx;
+		color: #666;
+		line-height: 46upx;
+		height: 46upx;
+		padding: 0 20upx;
+		border-radius: 23upx;
+		margin-left: 15upx;
+		margin-bottom: 20upx;
+		border: 1px solid #ccc;
+	}
+</style>
---
+++ music/pages/index/index.vue
@@ -0,0 +1,149 @@
+<template>
+	<view class="container">
+		<jmSearch v-on:flush_data="flush_data"></jmSearch>
+		<view class="">
+			<radio-group @change="switch_search">
+				<label>
+					<radio checked="true" value="migu" /><text>咪咕音乐</text>
+				</label>
+				<label>
+					<radio value="qq" /><text>QQ音乐</text>
+				</label>
+				<label>
+					<radio value="netease" /><text>网易云音乐</text>
+				</label>
+			</radio-group>
+		</view>
+		<view class="">
+			<button class="bt" size="mini" type="default" @click="get_list('19723756')">飙升榜</button>
+			<button class="bt" size="mini" type="default" @click="get_list('3779629')">新歌榜</button>
+			<button class="bt" size="mini" type="default" @click="get_list('2884035')">原创榜</button>
+			<button class="bt" size="mini" type="default" @click="get_list('3778678')">热歌榜</button>
+			<button class="bt" size="mini" type="default" @click="confirmDialog"><text class="button-text">链接</text></button>
+		</view>
+		<uni-popup id="dialogInput" ref="dialogInput" type="dialog">
+			<uni-popup-dialog mode="input" title="下载链接" :value="audio[now]" placeholder="请输入内容" @confirm="dialogInputConfirm"></uni-popup-dialog>
+		</uni-popup>
+		<scroll-view scroll-top="0" scroll-y="true" class="scroll-Y">
+			<uni-list v-for="(item, index) in dataList" :key=index>
+				<uni-list-item :class = "index == now ? 'addclass' : '' " :title="item.name" thumb-size="medium" :thumb="item.pic" :rightText="item.author" clickable  @click="playone(index)"></uni-list-item>
+			</uni-list>
+	    </scroll-view>
+		<imt-audio class="footer" continue :src="audio[now]" :duration="audio[now].duration" @prev="now = now === 0?audio.length-1:now-1" @next="now = now === audio.length-1?0:now+1"></imt-audio>
+	</view>
+</template>
+
+<script>
+	import jmSearch from "../../components/jm-search/jm-search.vue"
+	import imtAudio from 'components/imt-audio/imt-audio'
+	export default {
+		components: {jmSearch,imtAudio},
+		
+		data() {
+			return {
+				dataList: [],			//播放列表
+				audio: ['http://m7.music.126.net/20210511215751/f1d81bd47c23678520443b02c4845f14/ymusic/0052/5309/0152/782cc23c52dcb1ccbc0868ceaf2d56c4.mp3'],
+				now: 0,					//默认播放歌曲
+				search_type: "migu",	//默认搜索类型:migu
+			}
+		},
+		
+		methods: {
+			//获取链接
+			confirmDialog() {
+				this.$refs.dialogInput.open()
+			},
+			
+			//下载至服务器
+			dialogInputConfirm(done, val) {
+				this.value = val
+				setTimeout(() => {
+					uni.hideLoading()
+					// uni.request({
+					// 	url:'http://39.101.203.25/download/api',
+					// 	method:'POST',
+					// 	data:this.dataList[this.now],
+					// 	success: (res) => {
+					// 		console.log(res.statusCode)
+					// 	}
+					// });
+					done()
+				}, 0)
+			},
+			
+			//将search_type传入搜索子组件
+			switch_search(evt){
+				this.search_type = evt.detail["value"]
+			},        
+			
+			//搜索子组件返回歌曲，进行页面渲染
+			flush_data(r){
+				uni.request({
+					url:'http://39.101.203.25/' + this.search_type + '/api?keyword=' + r,
+					method:'GET',
+					data:{},
+					success: (res) => {
+						this.now = 0
+						this.dataList = res.data
+						this.audio = res.data.map(x => {return x.src})
+					}
+				});
+			},
+			
+			//播放选中歌曲
+			playone(msg){
+				this.now = msg
+			},
+			
+			//飙升榜...
+			get_list(s){
+				uni.request({
+					url:'http://39.101.203.25/netease/api?keyword=' + s,
+					method:'GET',
+					data:{},
+					success: (res) => {
+						this.dataList = res.data
+						this.audio = res.data.map(x => {return x.src})
+					}
+				});
+			},
+		},
+		
+		//打开页面时，初始化数据
+		beforeCreate() {
+			uni.request({
+				url:'http://39.101.203.25/netease/api?keyword=' + "抖音",
+				method:'GET',
+				data:{},
+				success: (res) => {
+					this.dataList = res.data
+					this.audio = res.data.map(x => {return x.src})
+				}
+			});
+		}
+	}
+</script>
+
+<style>
+	.scroll-Y {
+			height: 850rpx;
+		}
+		
+	.bt{
+		background-color: #ffe3d9;
+		color: #d91604;
+		text-align: center;
+		padding: 0.1% 0 0;
+		width: 20%;
+	}
+	
+	.addclass{
+		background-color: #ffe3d9;
+	}
+	
+	.footer{
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+	}
+</style>
